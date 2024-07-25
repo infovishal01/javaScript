77b8fb22ae0d50ad7a2adffcaf3412f54c885942
Vishal Kumar <106486681+infovishal01@users.noreply.github.com>
2024-07-25 19:16:09 +0000

    dataTypes

---
+++ 01-basic/02-dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs, not browser
+
+console.log( 3 
+    + 
+    3); // code readability shoud be high
+
+console.log("vishal")
+
+let name = "vishal"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bignit
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof "visha;l"); // String
+
+console.log(typeof null); // object
+
+console.log(typeof undefined); // Undefined
