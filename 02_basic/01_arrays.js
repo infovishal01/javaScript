93dd521a7c3f224bb8f39eba7b0928a08de9b7a3
Vishal Kumar <106486681+infovishal01@users.noreply.github.com>
2024-07-26 20:28:22 +0000

    array

---
+++ 02_basic/01_arrays.js
@@ -0,0 +1,39 @@
+// Array \\
+
+const myArr = [0,2,4,6,8,9]
+const myHerors = ["shaktiman", "naagraj"]
+
+const myArr2 = new Array(1,2,3,4)
+console.log(myArr[1]);
+
+
+// Array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr2.pop(2)
+
+// myArr.unshift(9);
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(1));
+
+// const newArr = myArr.join()
+
+// console.log(myArr)
+// console.log(newArr)
+// console.log(typeof newArr)
+
+// ***** Slice ***** \\
+
+console.log("A", myArr)
+
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1)
+console.log("B", myArr);
+
+const myn2 = myArr.slice(1, 3)
+console.log("C", myArr);
+console.log(myn2); 
